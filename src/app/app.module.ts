9232b13f0e41ca6d7127e0083cac0a94491a4dfa
Corentin Hoareau <corentinhoareaufr@gmail.com>
2022-04-29 16:20:43 +0400

    installs angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TicketCreateComponent } from './tickets/ticket-create/ticket-create/ticket-create.component';
@@ -10,7 +12,9 @@ import { TicketCreateComponent } from './tickets/ticket-create/ticket-create/tic
     TicketCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tickets/ticket-create/ticket-create/ticket-create.component.ts
+++ src/app/tickets/ticket-create/ticket-create/ticket-create.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ticket-create',
   templateUrl: './ticket-create.component.html',
   styleUrls: ['./ticket-create.component.css']
 })
-export class TicketCreateComponent implements OnInit {
+export class TicketCreateComponent {
+  inputVal = '';
+  issue = 'empty';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onAddTicket() {
+    this.issue = this.inputVal;
   }
-
 }
