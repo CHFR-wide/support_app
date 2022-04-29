2ab0b24ded3639b607445b36867f3d722341ce92
Corentin Hoareau <corentinhoareaufr@gmail.com>
2022-04-29 15:24:36 +0400

    Creates ticket creation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TicketCreateComponent } from './tickets/ticket-create/ticket-create/ticket-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketCreateComponent
   ],
   imports: [
     BrowserModule
